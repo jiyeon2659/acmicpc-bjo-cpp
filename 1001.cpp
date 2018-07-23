3837dbad3cac0323ae0f19e0acc7443c9fb7573d
jiyeon2659 <ddovobb69@gmail.com>
2018-07-22 20:23:37 -0700

    180722

---
+++ 1001.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int a, b, result;
+	cin >> a >> b;
+	result = a - b;
+	cout << result << endl;
+	return 0;
+}
---
+++ 7287.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "4" << endl << "jiyeon2659" << endl;
+	return 0;
+}
